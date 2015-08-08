3bd0aa8c9a398b03e41e8b4fdff3e4dd143026e7
Benjamin Guillet <benjamin.guillet@gmail.com>
2015-08-07 17:51:25 -0700

    Only print to stdout if DEBUG is set to true.

--- buffer.go
+++ buffer.go
@@ -2,7 +2,6 @@ package kinesis
 
 import (
 	"fmt"
-	"log"
 	"sync"
 	"text/template"
 
@@ -104,8 +103,8 @@ func (r *recordBuffer) Add(m *router.Message) error {
 		PartitionKey: aws.String(pKey),
 	})
 
-	log.Printf("kinesis: record added, stream name: %s, partition key: %s, length: %d\n",
-		*r.input.StreamName, pKey, len(r.input.Records))
+	debugLog(fmt.Sprintf("kinesis: record added, stream name: %s, partition key: %s, length: %d\n",
+		*r.input.StreamName, pKey, len(r.input.Records)))
 
 	return nil
 }
@@ -116,7 +115,7 @@ func (r *recordBuffer) Flush() error {
 	defer r.mutex.Unlock()
 
 	if r.count == 0 {
-		log.Printf("kinesis: nothing to flush, stream name: %s\n", *r.input.StreamName)
+		debugLog(fmt.Sprintf("kinesis: nothing to flush, stream name: %s\n", *r.input.StreamName))
 		return nil
 	}
 
@@ -127,8 +126,8 @@ func (r *recordBuffer) Flush() error {
 		return err
 	}
 
-	log.Printf("kinesis: buffer flushed, stream name: %s, records sent: %d, records failed: %d\n",
-		*r.input.StreamName, len(r.input.Records), *resp.FailedRecordCount)
+	debugLog(fmt.Sprintf("kinesis: buffer flushed, stream name: %s, records sent: %d, records failed: %d\n",
+		*r.input.StreamName, len(r.input.Records), *resp.FailedRecordCount))
 
 	return nil
 }
@@ -138,6 +137,6 @@ func (r *recordBuffer) reset() {
 	r.byteSize = 0
 	r.input.Records = make([]*kinesis.PutRecordsRequestEntry, 0)
 
-	log.Printf("kinesis: buffer reset, stream name: %s, length: %d\n",
-		*r.input.StreamName, len(r.input.Records))
+	debugLog(fmt.Sprintf("kinesis: buffer reset, stream name: %s, length: %d\n",
+		*r.input.StreamName, len(r.input.Records)))
 }
